bd91838ea5c1419732246e3ed053ea50c09c9b2c
andermsilva <andermarsil@gmail.com>
2023-04-02 21:39:09 -0300

    favicon, title e SVG

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ if (isset($_GET['enviar'])) {
   if ($_GET['enviar'] == '4') {
     $id = 4;
   }
+  if ($_GET['enviar'] == '5') {
+    $id = 5;
+  }
 }
 
 ?>
@@ -29,7 +32,7 @@ if (isset($_GET['enviar'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Trabalho PHP</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"><link rel="icon" href="php.png" type="image/x-icon" />
   <link rel="stylesheet" href="styles.css" />
 </head>
 
@@ -47,12 +50,13 @@ if (isset($_GET['enviar'])) {
             <li><button class='dropdown-item' name="enviar" type='submit' value="2">Soma dos 100 primeiros número</button></li>
             <li><button class='dropdown-item' name="enviar" type='submit' value="3">Sequencia Fibonacci </button></li>
             <li><button class='dropdown-item' name="enviar" type='submit' value="4"> Tabuada </button></li>
+            <li><button class='dropdown-item' name="enviar" type='submit' value="5"> Ordenar Vetor </button></li>
           </form>
         </ul>
       </div>
 
     </header>
-    <h1>Hello, world!</h1>
+    <h1 class="text-center text-danger">Trabalho 2 PHP <img src="icons8-php-logo-1200.svg" /></h1>
     <section class="ex border border-dark-subtle rounded-2">
 
       <div style="display: <?= (1 == $id) ? '' : 'none' ?> " class="border border-primary-subte text-center p-2 rounded">
@@ -155,27 +159,82 @@ if (isset($_GET['enviar'])) {
 
           <?php
           for ($i = 1; $i <= 100; $i++) {
-            if($i % 2 == 0)
+            if ($i % 2 == 0)
               $bg_cor = "bg-info";
-              else
+            else
               $bg_cor = "bg-info-subtle";
           ?>
-            <div class="item-container border border-secondary rounded text-info-emphasis <?=$bg_cor?> ">
-            <h6>Tabuada do <?=$i?></h6>
-            <?php 
-              for ($j=1; $j <= 10 ; $j++) { 
-                echo $i." X $j = ".($j*$i)."<br />";
+            <div class="item-container border border-secondary rounded text-info-emphasis <?= $bg_cor ?> ">
+              <h6>Tabuada do <?= $i ?></h6>
+              <?php
+              for ($j = 1; $j <= 10; $j++) {
+                echo $i . " X $j = " . ($j * $i) . "<br />";
               }
-            ?>
-            
+              ?>
             </div>
-
-
           <?php
-
           }
           ?>
+        </div>
+      </div>
 
+      <div style="display: <?= (5 == $id) ? '' : 'none' ?>" class=" text-center">
+        <h5 class="text-secondary border-bottom border-secondary">Ordenar Vetor...</h5>
+        <div>
+
+          <?php
+          $par = 0;
+          $impar = 0;
+          $nums = [];
+          for ($i = 1; $i < 51; $i++) {
+            $nums[$i] =$i;
+          }
+          shuffle($nums);
+          echo "<p class='alert alert-secondary text-info-emphasis' >Vetor: [ ";
+          foreach ($nums as $key => $value) {
+            echo $value;
+            if ($key != count($nums) - 1)
+              echo ", ";
+          }
+          echo " ] </p> ";
+          $menor = 0;
+          for ($j = 0; $j < count($nums); $j++) {
+
+            $i = $j + 1;
+
+            $k = $j;
+
+            while ($i < count($nums)) {
+              if ($nums[$i] < $nums[$k]) {
+                $k = $i;
+              }
+              $i++;
+            }
+            $menor = $nums[$j];
+            $nums[$j] = $nums[$k];
+            $nums[$k] = $menor;
+          }
+          echo "<p class='alert alert-success text-success-emphasis' >Vetor: [ ";
+          foreach ($nums as $key => $value) {
+
+            if ($value % 2 == 0) {
+              $par++;
+            } else {
+              $impar++;
+            }
+
+            echo $value;
+            if ($key != count($nums) - 1)
+              echo ", ";
+          }
+          echo " ] </p>";
+          echo " <p class='alert alert-info text-info-emphasis' >";
+          echo "  $par numeros pares e $impar números ímpares      </p>";
+
+          ?>
+
+        </div>
+      </div>
     </section>
 
   </main>
